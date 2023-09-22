efa117e93399a335de1674d74ea141172c31fdb6
crystalineddd <145733954+crystalineddd@users.noreply.github.com>
2023-09-22 11:59:49 +0800

    Add files via upload

---
+++ Activity1/userFather.py
@@ -0,0 +1,4 @@
+year=int(input("Enter the year now: "))
+birth=int(input("Enter your birthyear: "))
+age=year-birth
+print("Your age is:", age)
---
+++ Activity1/userInfo.py
@@ -0,0 +1,8 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+address=input("Enter your address: ")
+print(" ")
+print(name,"'s Information")
+print("Name:", name)
+print("Age:", age)
+print("Address:", address)
