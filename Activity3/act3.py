f9a7a2de2a3ec0a05c76c05bafca97bd64d9b84e
crystalineddd <145733954+crystalineddd@users.noreply.github.com>
2023-09-22 12:11:02 +0800

    Add files via upload

---
+++ Activity3/act3.py
@@ -0,0 +1,30 @@
+# 1.
+for x in range(5):
+    print("Advanced Computer Programming")
+
+# 2.
+print(" ")
+name=input("Enter your name: ")
+for _ in range(4):
+    print(name)
+
+# 3.
+print(" ")
+i=0
+while i<=9:
+    print(i)
+    i+=1
+
+# 4.
+print(" ")
+i=0
+while i<=6:
+    print(i)
+    i+=1
+
+# 5.
+print(" ")
+i=0
+while i<=9:
+    i+=1
+    print(i)
